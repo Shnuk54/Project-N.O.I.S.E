4fd0f1972d77ea2d8900017fe8f0d81164d4564a
Shnuk54 <dencik20332@mail.ru>
2022-08-06 20:07:25 +0300

    Немного доделал передвижение

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    
+    [Header("Walk and Sprint")]
+    [Range(0.01f, 10f)] [SerializeField] float _speed;
+    [Range(0.02f, 15f)] [SerializeField] float _sprintSpeed;
+    [Range(-10, 10)] [SerializeField] float _gravity = -5;
     CharacterController _charController;
-
-    [Range(0.01f,10f)]
-    [SerializeField] float _speed;
-
-    [Range(0.02f, 15f)]
-    [SerializeField] float _sprintSpeed;
-
-    [Range(0.5f,10)]
-    [SerializeField] float _mouseSens;
-
-    [Range(-10,10)]
-    [SerializeField] float _gravity = -5;
-
-    [SerializeField] float _maxXRot;
-    [SerializeField] float _minXRot;
-    private float _xRot;
-    private float _yRot;
     private Transform _transform;
+
+    [Header("Steps")]
+    [SerializeField] private float _stepLengthWalk;
+    [SerializeField] private float _stepLengthSprint;
+    [SerializeField] private bool _leftStep;
+    [SerializeField] private bool _rightStep;
+    private Vector3 _currentStepPos;
+   
+
+    [Header("Head")]
+
+    [Range(0,85)] [SerializeField] float _maxXRot;
+    [Range(-85, 0)] [SerializeField] float _minXRot;
+    [Range(0, 85)] [SerializeField] float _maxYRot;
+    [Range(-85, 0)] [SerializeField] float _minYRot;
+    [Range(0.5f, 10)] [SerializeField] float _mouseSens;
+    [SerializeField] Transform _headTransform;
+    private float _xRot;
+    private float _yHeadRot;
+    private float _yBodyRot;
+    
    
    
 
@@ -32,9 +41,14 @@ public class PlayerMovement : MonoBehaviour
     {
         _charController = GetComponent<CharacterController>();
         _transform = GetComponent<Transform>();
-       
+        _currentStepPos = this.transform.position;
+        
     }
-    
+
+    private void Update()
+    {
+        HandleStep();
+    }
     public void Move(){
         var verInput = InputHandler.instance.verInput;
         var horInput = InputHandler.instance.horInput;
@@ -48,13 +62,41 @@ public class PlayerMovement : MonoBehaviour
         if(InputHandler.instance.sprinting == false)dir = dir * _speed;
         if (InputHandler.instance.sprinting) dir = dir * _sprintSpeed;
         dir.y = _gravity;
-   
         _charController.Move(dir);
     }
      public void Rotation(){
-            _yRot += InputHandler.instance.mouseX * _mouseSens;
-            _xRot -= InputHandler.instance.mouseY * _mouseSens;
-            _xRot = Mathf.Clamp(_xRot, _minXRot, _maxXRot);
-            _transform.localRotation = Quaternion.Euler(_xRot, _yRot, 0);
+        _yHeadRot += InputHandler.instance.mouseX * _mouseSens;
+        _yBodyRot += InputHandler.instance.mouseX * _mouseSens;
+        _xRot -= InputHandler.instance.mouseY * _mouseSens;
+        _xRot = Mathf.Clamp(_xRot, _minXRot, _maxXRot);
+        _yHeadRot = Mathf.Clamp(_yHeadRot, _minYRot, _maxYRot);
+
+       if(_yBodyRot > _yHeadRot || _yBodyRot < _yHeadRot)
+        {
+            _transform.localRotation = Quaternion.Euler(0, Mathf.Lerp(_yBodyRot, _transform.localRotation.y, 0.1f * Time.deltaTime), 0);
+        }
+
+        _headTransform.localRotation = Quaternion.Euler(_xRot, _yHeadRot, 0);
+    }
+
+    private void HandleStep()
+    {
+        if(Vector3.Distance(_currentStepPos,_transform.position) > _stepLengthWalk)
+        {
+            _currentStepPos = _transform.position;
+            if (!_leftStep && !_rightStep) _leftStep = true;
+            if (_leftStep)
+            {
+                _leftStep = false;
+                _rightStep = true;
+                return;
+            }
+            if (_rightStep)
+            {
+                _leftStep = true;
+                _rightStep = false;
+                return;
+            }
+        }
     }
 }
